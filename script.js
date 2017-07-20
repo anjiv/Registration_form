bc8b88ccf7a8f2792d000ea797a4c8a79cbc6bf4
Anjali vijay <anjali.vijay@qed42.com>
2017-07-20 19:08:11 +0530

    Few validations added.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 (function($) {
   // Document ready event start
   $(document).ready(function() {
+
     $("input[type='radio']").change(function() {
       if ($(this).val() == "first") {
         $(".list").show();
@@ -18,6 +19,7 @@
     });
 
     $.validate({
+      form: '#registration-form',
       modules: 'security',
       onModulesLoaded: function() {
         var optionalConfig = {
@@ -35,16 +37,17 @@
         if (!$.formUtils.haltValidation) {
           $.notify({
             // options
-            icon: 'glyphicon glyphicon-success-sign',
-            title: 'Bootstrap notify',
-            message: 'Turning standard Bootstrap alerts into "notify" like notifications'
+            icon: 'glyphicon glyphicon-warning-sign',
+            title: "<strong>Successful</strong> ",
+            message: 'Submission successful'
           }, {
             // settings
             element: 'body',
             position: null,
+            type: "success",
             allow_dismiss: true,
             newest_on_top: false,
-            showProgressbar: true,
+            showProgressbar: false,
             placement: {
               from: "top",
               align: "right"
@@ -52,9 +55,8 @@
             offset: 20,
             spacing: 10,
             z_index: 1031,
-            delay: 10000,
+            delay: 3000,
             timer: 1000,
-            url_target: '_blank',
             mouse_over: null,
             animate: {
               enter: 'animated fadeInDown',
@@ -64,30 +66,39 @@
             onShown: null,
             onClose: null,
             onClosed: null,
-            type: 'minimalist',
             icon_type: 'class',
-            template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-success" role="alert">' +
+            template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
               '<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
               '<span data-notify="icon"></span> ' +
-              '<span data-notify="title">Successful</span> ' +
-              '<span data-notify="message">Form submitted</span>' +
+              '<span data-notify="title">{1}</span> ' +
+              '<span data-notify="message">{2}</span>' +
               '<div class="progress" data-notify="progressbar">' +
               '<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
               '</div>' +
+              '<a href="{3}" target="{4}" data-notify="url"></a>' +
               '</div>'
           });
-          $form.reset();
-          $form.find('input[type="submit"]').unbind('click');
         }
         return false;
-      }
+      },
 
+      onElementValidate: function(valid, $el, $form, errorMess) {
+        var $label = '$el.closest('.form - content ').find('
+        label ')';
+        console.log('Input ' + $el.attr('name') + ' is ' + (valid ? 'VALID' : 'NOT VALID'));
+        if (!valid) {
+          $el.closest('.form-content').find('label').addClass('error');
+        } else {
+          if ($label.hasClass('error')) {
+            $el.closest('.form-content').find('label').removeClass('error');
+          }
+        }
+      }
     });
 
     $('#datepicker').datepicker();
 
     $('.selectpicker').selectpicker();
-
     var availableTags = [
       "Science",
       "Medical",
